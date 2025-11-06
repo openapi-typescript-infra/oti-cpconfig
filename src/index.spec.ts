adae84f96ac1b6b037124553362823f78b08936e
Max Metral <max@pyralis.com>
2025-11-05 20:12:21 -0500

    fix: update test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,14 +5,13 @@ import { config } from './index.js';
 describe('Module exports', () => {
   test('should export expected elements', () => {
     expect(config).toBeInstanceOf(Function);
-    const configValue = config({});
+    const configValue = config();
     expect(configValue['.commitlintrc.yaml']).toBeDefined();
     expect(configValue['eslint.config.mts']).toBeDefined();
     expect(configValue['tsconfig.json']).toBeDefined();
     expect(configValue['tsconfig.build.json']).toBeDefined();
     expect(configValue['.prettierrc.yaml']).toBeDefined();
     expect(configValue['vitest.config.ts']).toBeDefined();
-    expect(configValue['.github/actionlint.yml']).toBeDefined();
     expect(configValue['tsconfig.tsup.json']).not.toBeDefined();
   });
 });
